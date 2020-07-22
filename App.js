e785a7d7ff2fc0884dd00e0ef288e69fd7eb3365
Hércules de Sousa <herculesdeso@gmail.com>
2020-07-22 14:36:37 -0300

    :sparkles: feat: introducing an image to the box

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import { AntDesign, MaterialIcons, Ionicons} from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <MaterialIcons name="filter-list" size={32} color="black" />
+        <MaterialIcons name="filter-list" size={32} color="black"/>
         <Ionicons name="ios-infinite" size={32} color="black" />
         <AntDesign name="search1" size={32} color="black" />
       </View>
 
       <ScrollView contentContainerStyle={styles.body}>
-        <View style={styles.box} />
+        <View style={styles.box}>
+          <Image source={require('./watches/datejust36.png')} style={styles.watchImage} />
+          <Text style={styles.watchName}>DATEJUST 36</Text>
+          <Text style={styles.watchType}>Classic</Text>
+          <Text style={styles.watchPrice}>R$ 51.850</Text>
+        </View>
         <View style={styles.box} />
         <View style={styles.box} />
         <View style={styles.box} />
@@ -36,15 +41,18 @@ const styles = StyleSheet.create({
 
   box: {
     width: 136,
-    height: 136,
-    backgroundColor: '#666',
-    borderWidth: 5,
-    borderColor: '#999',
-    margin: 16,
+    height: 174,
+    backgroundColor: '#f5f5f5',
+    marginBottom: 32,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    borderRadius: 24,
+    borderColor: '#806261',
+    borderWidth: 2,
   },
 
   header: {
-    paddingTop: 24,
+    paddingTop: 32,
     paddingHorizontal: 24,
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -54,6 +62,27 @@ const styles = StyleSheet.create({
     marginTop: 32,
     flexWrap: 'wrap',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
+  },
+
+  watchImage: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    top: 5
+  },
+
+  watchName: {
+    fontSize: 16
+  },
+
+  watchType: {
+    fontSize: 14,
+    color: '#d1d1d1'
+  },
+
+  watchPrice: {
+    color: '#806261',
+    marginBottom: 8,
   }
 });
