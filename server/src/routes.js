1ab60e64cc2ff9d8d8a0c9806c699e1855c82868
Hércules de Sousa <herculesdeso@gmail.com>
2020-08-11 18:37:31 -0300

    :sparkles: feat: starting server

---
+++ server/src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const routes = express.Router()
+
+routes.get('/pillow-images', (request, response) => {
+  return response.json({
+    "message": "Alright, everything alright",
+  })
+})
+
+module.exports = routes
