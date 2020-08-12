c0436d75c20334db532cae1481d8bf7f4698c7e3
Hércules de Sousa <herculesdeso@gmail.com>
2020-08-12 11:16:26 -0300

    :recycle: refactor: remaking the use of localhost

--- mobile/src/screens/pillowScreen/index.js
+++ mobile/src/screens/pillowScreen/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Image } from "react-native";
+import Constants from "expo-constants";
+const { manifest } = Constants;
 
 const PillowScreen = ({ route, navigation }) => {
   const { model } = route.params;
 
+  const [localHost, setLocalHost] = useState("");
+
+  useEffect(() => {
+    setLocalHost(manifest.hostUri.split(":")[0]);
+  }, []);
+
   return (
     <View>
       <Text>{model}</Text>
-      <Image source={{ uri: 'http://192.168.100.16:3333/pillow-images/pillowDatejust36.png' }} style={{ width: 80, height: 80 }} />
+      <Image
+        source={{
+          uri: `http://${localHost}:3333/pillow-images/pillowDatejust36.png`,
+        }}
+        style={{ width: 80, height: 80 }}
+      />
     </View>
   );
 };
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const routes = express.Router();
 
-routes.use("/pillow-images", express.static(__dirname + '/assets/pillowWatches'))
+routes.use(
+  "/pillow-images",
+  express.static(__dirname + "/assets/pillowWatches")
+);
 
 routes.get("/", (request, response) => {
   return response.json({
