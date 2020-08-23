c93d9f983e4e03501e3b0b38d17ff5b360ebc0a5
Hércules de Sousa <herculesdeso@gmail.com>
2020-08-23 12:06:36 -0300

    :sparkles: feat: adding buttons

--- mobile/src/screens/pillowScreen/index.js
+++ mobile/src/screens/pillowScreen/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity, Button } from "react-native";
 
 import { getLocalHost } from "../../utils";
 import Header from "../../components/header";
@@ -10,6 +10,9 @@ const PillowScreen = ({ route }) => {
 
   const [localHost, setLocalHost] = useState("");
   const [pillowColor, setPillowColor] = useState("gray");
+  const [bgGray, setBgGray] = useState(true);
+  const [bgBlack, setBgBlack] = useState(false);
+  const [bgBlue, setBgBlue] = useState(false);
 
   useEffect(() => {
     setLocalHost(getLocalHost());
@@ -20,7 +23,23 @@ const PillowScreen = ({ route }) => {
     return watch;
   }
 
-  
+  function handleChangeOfPillowColor(color) {
+    if (color === "gray") {
+      setBgGray(true)
+      setBgBlack(false)
+      setBgBlue(false)
+    }
+    if (color === "black") {
+      setBgGray(false)
+      setBgBlack(true)
+      setBgBlue(false)
+    }
+    if (color === "blue") {
+      setBgGray(false)
+      setBgBlack(false)
+      setBgBlue(true)
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -35,6 +54,57 @@ const PillowScreen = ({ route }) => {
           }}
           style={styles.pillowImage}
         />
+
+        <View style={{ marginVertical: 24 }}>
+          <Text>Choose the color of the pillow:</Text>
+        </View>
+
+        <View style={{ flexDirection: "row" }}>
+
+          <TouchableOpacity 
+            style={{ 
+              backgroundColor: bgBlack ? "black" : "#f5f5f5", 
+              padding: 8, 
+              borderRadius: 32, 
+              borderWidth: 1,
+              borderColor: "black",
+              marginHorizontal: 8,
+            }}
+            onPress={() => {handleChangeOfPillowColor("black")}}
+          >
+            <View style={{ backgroundColor: "black", width: 40, height: 40, borderRadius:32 }} />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{ 
+              backgroundColor: bgGray ? "gray" : "#f5f5f5", 
+              padding: 8, 
+              borderRadius: 32, 
+              borderWidth: 1,
+              borderColor: "black",
+              marginHorizontal: 8,
+            }}
+            onPress={() => {handleChangeOfPillowColor("gray")}}
+          >
+            <View style={{ backgroundColor: "gray", width: 40, height: 40, borderRadius:32 }} />
+          </TouchableOpacity>
+          
+          <TouchableOpacity 
+            style={{ 
+              backgroundColor: bgBlue ? "blue" : "#f5f5f5", 
+              padding: 8, 
+              borderRadius: 32, 
+              borderWidth: 1,
+              borderColor: "black",
+              marginHorizontal: 8,
+            }}
+            onPress={() => {handleChangeOfPillowColor("blue")}}
+          >
+            <View style={{ backgroundColor: "blue", width: 40, height: 40, borderRadius:32 }} />
+          </TouchableOpacity>
+
+        </View>
+
       </View>
 
       <BottomButtons buttonText="Finish buying" />
